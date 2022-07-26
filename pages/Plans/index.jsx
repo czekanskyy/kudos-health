69aba2ff46b47dc57f31520533770e45d50264e0
Dominik Czekański <83006539+czekens@users.noreply.github.com>
2022-07-26 01:23:53 +0100

    Day 0 changes

---
+++ pages/Plans/index.jsx
@@ -0,0 +1,9 @@
+import PlanBox from './PlanBox';
+
+export default function Plans() {
+  return (
+    <section className="w-full flex justify-center">
+      <PlanBox planName="Free" planColor="#9b59b6"></PlanBox>
+    </section>
+  );
+}
