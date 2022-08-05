c3b5da54118e44a67ba6ddb1c56de9850bbb32ec
Dominik Czekański <83006539+czekens@users.noreply.github.com>
2022-08-05 12:35:48 +0100

    input box changed

--- components/InputBox.jsx
+++ components/InputBox.jsx
@@ -1,18 +1,18 @@
 const InputBox = ({ onChangeHandler, display }) => {
   return (
-    <section className='bg-gradient-to-br from-blue-400 to-blue-600 w-11/12 md:w-[42rem] rounded-xl shadow-lg text-white flex flex-col items-center p-8 gap-4'>
+    <section className='rounded-md shadow-lg bg-white w-11/12 md:w-[42rem] text-blue-500 flex flex-col items-center p-8 gap-4 mb-6'>
       <h2 className='text-base md:text-2xl font-semibold'>Choose your subscription package</h2>
-      <hr className='w-1/4 mt-2 border-t-2 border-white' />
-      <p className='text-center text-xs md:text-sm text-gray-50'>How many Employee do you want to give access to programme</p>
+      <hr className='w-1/4 mt-2 border-t-2 border-blue-500' />
+      <p className='text-center text-xs md:text-sm text-blue-400'>How many Employee do you want to give access to programme</p>
       <input
         type='number'
         name='numEmployees'
         id='numEmployees'
-        className='w-2/5 p-2 bg-transparent outline-none border border-white rounded-md text-center placeholder:text-white transition-all focus:w-72'
+        className='w-2/5 p-2 bg-transparent outline-none border border-blue-500 rounded-md text-center placeholder:text-blue-400 transition-all focus:w-72'
         placeholder='Enter here'
         onChange={onChangeHandler}
       />
-      <span className={`bg-red-500 px-2 p-1 rounded-md text-xs -mt-2 ${display}`}>Maximum value is 999 999</span>
+      <span className={`bg-red-500 px-2 p-1 rounded-md text-xs -mt-2 text-white ${display}`}>Maximum value is 999 999</span>
       <a href='#features' className='text-xs mt-2'>
         See features avalible in plans
       </a>
